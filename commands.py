7ca1d36e44b786e152364a0979074fecaa5bde9e
AlbertLara <thepivot88@gmail.com>
2021-11-29 08:00:51 +0100

    .

--- commands.py
+++ commands.py
@@ -1,15 +1,20 @@
 from discord.ext import commands
+from discord.ext.commands import Cog, Bot, command, Context
 
-class SomeCommands(commands.Cog): 
+class SomeCommands(Cog): 
     """A couple of simple commands.""" 
     
-    def __init__(self, bot: commands.Bot): 
+    def __init__(self, bot: Bot): 
         self.bot = bot
         
-    @commands.command(name="ping")
-    async def ping(self, ctx: commands.Context):
+    @command(name="ping")
+    async def ping(self, ctx: Context):
         """Get the bot's current websocket latency."""
         await ctx.send(f"Pong!")
+        
+    @command(name="stats")
+    async def stats(self, ctx: Context):
+        await ctx.send("Here are the stats.")
         
 def setup(bot: commands.Bot):
     bot.add_cog(SomeCommands(bot))
